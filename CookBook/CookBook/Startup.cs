fb0953da98fb71390d927f73125c0181a65d24c1
Miłosz Robak <milosz@wandlee.com>
2022-01-13 19:56:14 +0100

    insert data to db only if user table is empty

--- CookBook/CookBook/Startup.cs
+++ CookBook/CookBook/Startup.cs
@@ -53,101 +53,105 @@ namespace CookBook
                     var dbContext = serviceScope.ServiceProvider.GetRequiredService<CookBookContext>();
 
                     dbContext.Database.EnsureCreated();
-
-                    User admin = new(){ Login = "root", Password = "toor", Role = "ADMIN" };
-                    User user = new(){ Login = "user", Password = "resu", Role = "USER" };
-
-                    var users = new User[] { admin, user };
-
-                    Category testCategory = new()
+                    if (!dbContext.Users.Any())
                     {
-                        Name = "TestCategory"
-                    };
+                        User admin = new() { Login = "root", Password = "toor", Role = "ADMIN" };
+                        User user = new() { Login = "user", Password = "resu", Role = "USER" };
 
-                    Ingredient testIngredient1 = new()
-                    {
-                        Name = "TestIngredient1"
-                    };
-                    Ingredient testIngredient2 = new()
-                    {
-                        Name = "TestIngredient2"
-                    };
-                    Ingredient testIngredient3 = new()
-                    {
-                        Name = "TestIngredient3"
-                    };
+                        var users = new User[] { admin, user };
 
-                    var ingredients = new Ingredient[] { testIngredient1, testIngredient2, testIngredient3 };
+                        Category testCategory = new()
+                        {
+                            Name = "TestCategory"
+                        };
 
-                    Measure testMeasure1 = new() {
-                        Name = "g"
-                    };
+                        Ingredient testIngredient1 = new()
+                        {
+                            Name = "TestIngredient1"
+                        };
+                        Ingredient testIngredient2 = new()
+                        {
+                            Name = "TestIngredient2"
+                        };
+                        Ingredient testIngredient3 = new()
+                        {
+                            Name = "TestIngredient3"
+                        };
 
-                    Measure testMeasure2 = new()
-                    {
-                        Name = "L"
-                    };
+                        var ingredients = new Ingredient[] { testIngredient1, testIngredient2, testIngredient3 };
 
-                    var measures = new Measure[] { testMeasure1, testMeasure2 };
+                        Measure testMeasure1 = new()
+                        {
+                            Name = "g"
+                        };
 
-                    RecipeIngredient testRecipeIngredient1 = new()
-                    {
-                        Ingredient = testIngredient1,
-                        Measure = testMeasure1,
-                        Ammount = 200
-                    };
-                    RecipeIngredient testRecipeIngredient2 = new()
-                    {
-                        Ingredient = testIngredient2,
-                        Measure = testMeasure2,
-                        Ammount = 200
-                    };
-                    RecipeIngredient testRecipeIngredient3 = new()
-                    {
-                        Ingredient = testIngredient3,
-                        Measure = testMeasure2,
-                        Ammount = 200
-                    };
+                        Measure testMeasure2 = new()
+                        {
+                            Name = "L"
+                        };
 
-                    List<RecipeIngredient> recipeIngredients = new()
-                    {
-                        testRecipeIngredient1,
-                        testRecipeIngredient2,
-                        testRecipeIngredient3
-                    };
+                        var measures = new Measure[] { testMeasure1, testMeasure2 };
 
-                    Recipe testRecipe = new() {
-                        Name = "TestRecipe",
-                        Category = testCategory,
-                        RecipeIngredients = recipeIngredients,
-                        Owner = user,
-                        CookingTime = new(1, 30, 0)
-                };
+                        RecipeIngredient testRecipeIngredient1 = new()
+                        {
+                            Ingredient = testIngredient1,
+                            Measure = testMeasure1,
+                            Ammount = 200
+                        };
+                        RecipeIngredient testRecipeIngredient2 = new()
+                        {
+                            Ingredient = testIngredient2,
+                            Measure = testMeasure2,
+                            Ammount = 200
+                        };
+                        RecipeIngredient testRecipeIngredient3 = new()
+                        {
+                            Ingredient = testIngredient3,
+                            Measure = testMeasure2,
+                            Ammount = 200
+                        };
 
-                    foreach (User u in users)
-                    {
-                        dbContext.Users.Add(u);
+                        List<RecipeIngredient> recipeIngredients = new()
+                        {
+                            testRecipeIngredient1,
+                            testRecipeIngredient2,
+                            testRecipeIngredient3
+                        };
+
+                        Recipe testRecipe = new()
+                        {
+                            Name = "TestRecipe",
+                            Category = testCategory,
+                            RecipeIngredients = recipeIngredients,
+                            Owner = user,
+                            CookingTime = new(1, 30, 0)
+                        };
+
+                        foreach (User u in users)
+                        {
+                            dbContext.Users.Add(u);
+                        }
+
+
+                        foreach (Ingredient i in ingredients)
+                        {
+                            dbContext.Ingredients.Add(i);
+                        }
+
+                        foreach (Measure m in measures)
+                        {
+                            dbContext.Measures.Add(m);
+                        }
+
+                        foreach (RecipeIngredient ri in recipeIngredients)
+                        {
+                            dbContext.RecipeIngredients.Add(ri);
+                        }
+
+                        dbContext.Recipes.Add(testRecipe);
+
+                        dbContext.SaveChanges();
                     }
-
-
-                    foreach (Ingredient i in ingredients)
-                    {
-                        dbContext.Ingredients.Add(i);
-                    }
-                    
-                    foreach (Measure m in measures)
-                    {
-                        dbContext.Measures.Add(m);
-                    }
-
-                    foreach (RecipeIngredient ri in recipeIngredients)
-                    {
-                        dbContext.RecipeIngredients.Add(ri);
-                    }
-
-                    dbContext.Recipes.Add(testRecipe);
-
-                    dbContext.SaveChanges();
 
                 }
 
